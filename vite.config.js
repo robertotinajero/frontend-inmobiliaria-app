a5ac8f24f4c72a91fc0c1ef1e0518e89d2c72996
Roberto Carlos Tinajero Reyes <rtinajero@inprotelecom.com>
2025-08-11 19:58:40 -0600

    Ajustes para production

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { createHash } from 'crypto'
+
+// Polyfill para evitar error de crypto.hash
+if (!('hash' in crypto)) {
+  crypto.hash = (input) => createHash('sha256').update(input).digest('hex')
+}
 
 // https://vite.dev/config/
 export default defineConfig({
